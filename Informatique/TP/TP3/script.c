264ab3f40a285d0c2c51a6e590a51eded7d711d5
WIDEROZ <roftelio001@gmail.com>
2024-10-08 11:22:12 +0200

    obsidian-auto-push : 2024-10-08 11:22:12

--- Informatique/TP/TP3/script.c
+++ Informatique/TP/TP3/script.c
@@ -13,8 +13,63 @@ int* indicatrice_positions(int T[], int N, int k){
     for (int i1 = 0; i1 < k; i1++){
         int j1 = T1[i1];
         T1[j1] = 0;
+        if (j1+(k-i1)< N)
+        {
+            T[j1+(k-i1)] = 0;
+        }
+        if (j1+(k-i1)>=0)
+        {
+            T[j1-(k-i1)] = 0;
+        }
+        return T1;
+    }
+}
+
+
+int nb_positions(int T1[], int N){
+    int c= 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (T1[i] == 1)
+        {
+            c++;
+        }
         
     }
+    return c;
     
+}
+
+int* indices_positions(int T1[], int N){
+    int n = nb_positions(T1, N);
+    int* T2 = malloc(sizeof(int)*n);
+
+    int c= 0;
+    for (int i = 0; i < N; i++)
+    {
+        if(T1[i] == 1){
+            T2[c] = i;
+            c++;
+        }
+    }
+    return T2;
     
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+    int N;
+    printf("Valeur de n : \n");
+    scanf("%d", &N);
+
+    int k = 0;
+    int *t_sol = malloc(sizeof(int)*N);
+    for (int i = 0; i < N; i++)
+    {
+        t_sol[i] = 0;
+    }
+    
+
+    return 0;
 }
