6d632789ba0e6cc79be30bc4fd9c514c3ec59690
WIDEROZ <roftelio001@gmail.com>
2024-11-05 10:32:55 +0100

    obsidian-auto-push : 2024-11-05 10:32:55

--- Informatique/TP/TP4/TP4.ml
+++ Informatique/TP/TP4/TP4.ml
@@ -2,3 +2,14 @@ open Queue
 open Stack
 type graphe_oriente = int list array ;;
 let ordre (g : graphe_oriente) : int = Array.length g ;;
+
+let rec parcours_largeur (g : graphe_oriente) (source :
+int) : int * int array = 
+  let file = Queue.create() in
+    for i = 0 to (ordre g) do
+      match g.(i) with
+      | [] -> failwith "ce n'est pas une matrice d'adjacence"
+      | t::s -> (file.enqueue t);
+
+    done;;
+  ;;
