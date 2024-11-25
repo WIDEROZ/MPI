d6bbb55eb9a8a0e629ecb7c3c0d8f2a5de8a6d45
WIDEROZ <roftelio001@gmail.com>
2024-11-25 16:44:17 +0100

    obsidian-auto-push : 2024-11-25 16:44:17

--- Informatique/TP/TP6/script.ml
+++ Informatique/TP/TP6/script.ml
@@ -1,4 +1,10 @@
-type regex = Vide | Epsilon | Lettre of char | Union of regex * regex | Concat of regex*regex | Etoile of char;;
+type regex =
+    Vide
+  | Epsilon
+  | Lettre of char
+  | Union of regex * regex
+  | Concat of regex*regex
+  | Etoile of char;;
 
 type ('a, 'b) automate =
     {
@@ -10,3 +16,15 @@ type ('a, 'b) automate =
     }
   ;;
 
+type regex_lin = 
+    Vide
+  | Epsilon
+  | Lettre of (char * int)
+  | Union of regex_lin * regex_lin
+  | Concat of regex_lin * regex_lin
+  | Etoile of regex_lin
+;;
+
+
+let lineariser (r:regex) : regex_lin = 
+  
