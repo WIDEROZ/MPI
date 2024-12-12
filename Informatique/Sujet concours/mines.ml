cb1dade45fc20b2a92772f6a1b827cae6e75d3e4
WIDEROZ <roftelio001@gmail.com>
2024-12-12 17:42:56 +0100

    obsidian-auto-push : 2024-12-12 17:42:56

--- Informatique/Sujet concours/mines.ml
+++ Informatique/Sujet concours/mines.ml
@@ -1,8 +1,10 @@
+let verif m = 
+  
 let parseV (w : char list) : string * char list =
   match w with
   | [] -> failwith "syntax error"
   | l -> let rec isInGram v = 
     match v with
     | [] -> failwith "syntax error"
-    | h::q -> (if (h = '#') then [h] else (if (h = 'a' || h = 'b') then h::(isInGram q) else failwith "syntax error") )
-  in isInGram l
+    | h::q -> (if (h = '#') then (verif q; String.make 1 h) else (if (h = 'a' || h = 'b') then ((String.make 1 h) ^ (isInGram q)) else failwith "syntax error") )
+  in (isInGram l, )
