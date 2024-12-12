ae25bb4b2666bf4b510fd542e99d12b8803ebfab
WIDEROZ <roftelio001@gmail.com>
2024-12-12 17:22:56 +0100

    obsidian-auto-push : 2024-12-12 17:22:56

--- Informatique/Sujet concours/mines.ml
+++ Informatique/Sujet concours/mines.ml
@@ -1,2 +1,7 @@
 let parseV (w : char list) : string * char list =
-  
+  match w with
+  | [] -> failwith "syntax error"
+  | t::s -> let rec isInGram v = 
+    match v with
+    | [] -> 0
+    | t::s
