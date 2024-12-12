4fd08ab58124c2387580055054fab5bf86a561aa
WIDEROZ <roftelio001@gmail.com>
2024-12-12 17:52:56 +0100

    obsidian-auto-push : 2024-12-12 17:52:56

--- Informatique/Sujet concours/mines.ml
+++ Informatique/Sujet concours/mines.ml
@@ -1,10 +1,23 @@
-let verif m = 
-  
+let rec inTerm q = 
+  match q with
+  | [] -> true
+  | t::s -> if (t!='a' && t!='b') then false else inTerm s 
+;;
 let parseV (w : char list) : string * char list =
-  match w with
-  | [] -> failwith "syntax error"
-  | l -> let rec isInGram v = 
-    match v with
+  let verif = ref false in
+  let s = ref [] in
+    match w with
     | [] -> failwith "syntax error"
-    | h::q -> (if (h = '#') then (verif q; String.make 1 h) else (if (h = 'a' || h = 'b') then ((String.make 1 h) ^ (isInGram q)) else failwith "syntax error") )
-  in (isInGram l, )
+    | l -> let rec isInGram v = 
+      match v with
+      | [] -> failwith "syntax error"
+      | h::q -> (if (h = '#') then (verif := inTerm q; s := q; String.make 1 h) else (if (h = 'a' || h = 'b') then ((String.make 1 h) ^ (isInGram q)) else failwith "syntax error") )
+    in 
+    if (!verif) then 
+      (isInGram l, !s)
+    else
+      failwith "sytaxe incorrecte"
+
+    ;;
+
+parseV ['a'; 'a'; 'b';'#'; 'b'; 'a'];;
