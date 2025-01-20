dd770c6ae2d81c1c1859a67d821c3fa88c321903
WIDEROZ <roftelio001@gmail.com>
2025-01-20 15:42:52 +0100

    obsidian-auto-push : 2025-01-20 15:42:52

--- TIPE/Programme/Lib/Evolution.c
+++ TIPE/Programme/Lib/Evolution.c
@@ -1,24 +1,46 @@
 #include "Evolution.h"
 
-int rechercheEtatCellsAutour(int X, int Y, ){
+int nbreCellulesAutour(matrix mat, int X, int Y){
+    int nbre = 0;
+    for (int l = -1; l < 2; l++)
+    {
+        if (0 <= X+l <= mat.width)
+        {
+            for (int k = -1; k < 2; k++)
+            {
+                if ((0 <= Y+k <= mat.height) && (k != 0 && l != 0))
+                {
+                    nbre += mat.mat[X+l][Y+k];
+                }
+            }
+        }
+    }
 
+    return nbre;
 }
 
-
-
 void evolution(Var *var){
+    matrix NEW_XY_CASE_MAT = CREATE_MATRIX(CASE_NUMBER_WIDTH, CASE_NUMBER_HEIGHT);
     for (int i = 0; i < CASE_NUMBER_WIDTH; i++)
     {
         for (int j = 0; j < CASE_NUMBER_HEIGHT; j++)
         {
-            int isCaseLiving = var->XY_CASE_MAT[i][j];
-            if (isCaseLiving)
+            int nbreCellAutour = nbreCellulesAutour(var->XY_CASE_MAT, i, j);
+            if (nbreCellAutour = 3)
             {
-                
+                NEW_XY_CASE_MAT.mat[i][j] = 1-(var->XY_CASE_MAT[i][j]);
             }
+            else if (condition)
+            {
+                /* code */
+            }
+            
             
         }
         
     }
-    
+
 }
+
+
+
