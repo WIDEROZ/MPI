922d53f73b1dd346e056231f208eb21ea9edfd35
WIDEROZ <roftelio001@gmail.com>
2025-01-20 15:32:51 +0100

    obsidian-auto-push : 2025-01-20 15:32:51

--- TIPE/Programme/Lib/Evolution.c
+++ TIPE/Programme/Lib/Evolution.c
@@ -1,10 +1,21 @@
 #include "Evolution.h"
 
+int rechercheEtatCellsAutour(int X, int Y, ){
+
+}
+
+
+
 void evolution(Var *var){
     for (int i = 0; i < CASE_NUMBER_WIDTH; i++)
     {
         for (int j = 0; j < CASE_NUMBER_HEIGHT; j++)
         {
+            int isCaseLiving = var->XY_CASE_MAT[i][j];
+            if (isCaseLiving)
+            {
+                
+            }
             
         }
         
