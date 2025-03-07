27f6a1416616ae04d8017443100da36f380a5121
WIDEROZ <roftelio001@gmail.com>
2025-03-07 13:46:46 +0100

    obsidian-auto-push : 2025-03-07 13:46:45

--- Informatique/TP/TP4/TP4.ml
+++ Informatique/TP/TP4/TP4.ml
@@ -175,22 +175,23 @@ type arbre = E | N of int * arbre * arbre;;
 
 
 let ajout_tas (tas:int array) (v:int) = 
-  if (tas.(0) == -1) then tas.(0) <- v
-  else
-  for i = 1 to (Array.length tas) do
-    if ((tas.((Array.length tas)-i) != -1) && tas.((Array.length tas)-i+1) == -1) then
-      tas.((Array.length tas)-i+1) <- v
-  done;
-  let rec aux i = 
-    match i with
-    | 0 -> tas.(0) <- tas.(0)
-    | _ -> if v > tas.((i-1)/2) then 
-             (tas.(i)<- tas.((i-1)/2); tas.((i-1)/2) <- v; aux )
-
-
+  let index = ref 0 in
+    if (tas.(0) == -1) then
+      tas.(0) <- v
+    else
+      for i = 1 to (Array.length tas) do
+        if ((tas.((Array.length tas)-i) != -1) && tas.((Array.length tas)-i+1) == -1) then
+          (tas.((Array.length tas)-i+1) <- v; index := (Array.length tas)-i+1)
+      done;
+    let rec aux i = 
+      match i with
+      | 0 -> tas.(0) <- tas.(0)
+      | _ -> if v > tas.((i-1)/2) then 
+              (tas.(i)<- tas.((i-1)/2); tas.((i-1)/2) <- v; aux ((i-1)/2))
+    in aux (!index)
 ;;
 
-let t = [||];;
+let t = [|-1; -1; -1; -1|];;
 
-let new_tas = ajout_tas t 15;;
-
+ajout_tas t 15;;
+t;;
