7a8dfb6a7421dcab420dbd5136b009f86d3e1703
WIDEROZ <roftelio001@gmail.com>
2025-03-10 19:40:14 +0100

    obsidian-auto-push : 2025-03-10 19:40:14

--- Informatique/Cours/Chapitre 9/script.c
+++ Informatique/Cours/Chapitre 9/script.c
@@ -1,1 +1,26 @@
-#include <pthread>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+int x=0;
+
+void *f(void *arg){
+    for (int i = 0; i <= 1000; i++)
+    {
+        x++;
+    }
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t t1;
+    pthread_t t2;
+
+    pthread_create(&t1, NULL, )
+    pthread_create(&t1, NULL, )
+    pthread_join(&)
+    pthread_join(&)
+
+    return 0;
+}
