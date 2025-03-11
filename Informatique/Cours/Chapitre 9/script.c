501017c695cc90b64e7d4892fc1952c5e56bd13e
WIDEROZ <roftelio001@gmail.com>
2025-03-11 10:45:02 +0100

    obsidian-auto-push : 2025-03-11 10:45:02

--- Informatique/Cours/Chapitre 9/script.c
+++ Informatique/Cours/Chapitre 9/script.c
@@ -17,10 +17,10 @@ int main(int argc, char const *argv[])
     pthread_t t1;
     pthread_t t2;
 
-    pthread_create(&t1, NULL, )
-    pthread_create(&t1, NULL, )
-    pthread_join(&)
-    pthread_join(&)
+    pthread_create(&t1, NULL, f, "A");
+    pthread_create(&t2, NULL, f, "B");
+    pthread_join(&t1, NULL);
+    pthread_join(&t2, NULL);
 
     return 0;
 }
