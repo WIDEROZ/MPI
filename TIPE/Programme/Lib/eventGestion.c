7c9349063d1896efd19f1612fcee91bed5fdd619
WIDEROZ <roftelio001@gmail.com>
2025-06-06 10:57:07 +0200

    obsidian-auto-push : 2025-06-06 10:57:07

--- TIPE/Programme/Lib/eventGestion.c
+++ TIPE/Programme/Lib/eventGestion.c
@@ -2,159 +2,154 @@
 
 int velocity = 500;
 
+
 SDL_bool GESTION(Var *var, SDL_Event event, bool* isMouseButtonPressed, bool* MOUSE_MOVING){
     while (SDL_PollEvent(&event)){ // Elle va lire tout les évènements
         /*on a aussi : SDL_WaitEvent(&event) mais ca bloque la fenêtre*/
-    switch (event.type) // Le switch c'est comme un match en ocaml mais que pour les int
+        switch (event.type) // Le switch c'est comme un match en ocaml mais que pour les int
+    {
+        case SDL_KEYDOWN :
+            
+            switch (event.key.keysym.sym)
             {
-                case SDL_KEYDOWN :
-                    
-                    switch (event.key.keysym.sym)
-                    {
-                    
-                    case SDLK_SPACE:
-                        var->KEY_DOWN_STATUS[SDLK_b] = 1;
-                        evolution(var);
-                        continue;
-                    
-                    case SDLK_RETURN:
-                        // Trigger
-                        if (var->KEY_DOWN_STATUS[SDLK_RETURN] == 0){
-                            var->KEY_DOWN_STATUS[SDLK_RETURN] = 1;
-                        }
-                        else{
-                            var->KEY_DOWN_STATUS[SDLK_RETURN] = 0;
-                        }
-                        
-                        continue;
-
-                    case SDLK_PLUS:
-                        velocity += 10;
-                        continue;
-                    
-                    case SDLK_MINUS:
-                        velocity -= 10;
-                        continue;
-
-                    case SDLK_c:
-                        var->KEY_DOWN_STATUS[SDLK_c] = 1;
-                        GRID_DISPLAY_CREATION(var);
-                        continue;
-
-                    case SDLK_LCTRL:
-                        var->KEY_DOWN_STATUS[SDLK_LCTRL] = 1;
-                        continue;
-
-                    case SDLK_z:
-                        var->KEY_DOWN_STATUS[SDLK_LCTRL] = 1;
-                        if (var->KEY_DOWN_STATUS[SDLK_LCTRL] == 1){
-                            // REMETTRE LA GRILLE AU TABLEAU ENTREGISTRÉ LORS del'activation de ENTER
-                        }
-                        continue;
-
+            
+            case SDLK_SPACE:
+                var->KEY_DOWN_STATUS[SDLK_b] = 1;
+                evolution(var);
+                continue;
+            
+            case SDLK_RETURN:
+                // Trigger
+                if (var->KEY_DOWN_STATUS[SDLK_RETURN] == 0){
+                    var->KEY_DOWN_STATUS[SDLK_RETURN] = 1;
+                }
+                else{
+                    var->KEY_DOWN_STATUS[SDLK_RETURN] = 0;
+                }
                 
-                    default:
-                        
-                        continue;
-                    }
+                continue;
+
+            case SDLK_PLUS:
+                velocity += 10;
+                continue;
+            
+            case SDLK_MINUS:
+                velocity -= 10;
+                continue;
+
+            case SDLK_c:
+                var->KEY_DOWN_STATUS[SDLK_c] = 1;
+                GRID_DISPLAY_CREATION(var);
+                continue;
+
+            case SDLK_LCTRL:
+                var->KEY_DOWN_STATUS[SDLK_LCTRL] = 1;
+                continue;
+
+            case SDLK_z:
+                var->KEY_DOWN_STATUS[SDLK_LCTRL] = 1;
+                if (var->KEY_DOWN_STATUS[SDLK_LCTRL] == 1){
+                    // REMETTRE LA GRILLE AU TABLEAU ENTREGISTRÉ LORS del'activation de ENTER
+                }
+                continue;
+
+        
+            default:
                 
-                case SDL_KEYUP : 
-                    switch (event.key.keysym.sym)
-                    {
-
-                    case SDLK_b:
-                        var->KEY_DOWN_STATUS[SDLK_b] = 0;
-                        continue;
-
-                    case SDLK_c:
-                        var->KEY_DOWN_STATUS[SDLK_c] = 0;
-                        continue;
-
-                    
-                    case SDLK_q:
-                        var->KEY_DOWN_STATUS[SDLK_q] = 0;
-                        continue;
-
-                    case SDLK_d:
-                        var->KEY_DOWN_STATUS[SDLK_d] = 0;
-                        continue;
-                    
-                    case SDLK_z:
-                        var->KEY_DOWN_STATUS[SDLK_z] = 0;
-                        continue;
-
-                    case SDLK_s:
-                        var->KEY_DOWN_STATUS[SDLK_s] = 0;
-                        continue;
-                    
-                    case SDLK_LCTRL:
-                        var->KEY_DOWN_STATUS[SDLK_LCTRL] = 0;
-                        continue;
-                    
-                    
-                    default:
-                        continue;
-                    }
-
-                case SDL_MOUSEMOTION :
-                    //printf("Coordonée : (%d, %d) \n", event.motion.x, event.motion.y);
-                    //printf("Vitesse : (%d, %d) \n", event.motion.xrel, event.motion.yrel);
-                    
-                    // ----- Actualisation du déplacement de la caméra avec la souris ----- //
-                    if(*isMouseButtonPressed && event.motion.x <= GRID_DISP_WIDTH && event.motion.y <= GRID_DISP_HEIGHT){
-                        MOVE_CAMERA_MOUSE(var->renderer, var->texture, var->camera, event.motion.xrel, event.motion.yrel);
-                        *MOUSE_MOVING = true;
-                    }
-                    
-                    
-                    
-                    
-                    
-                    continue;
-
-                case SDL_MOUSEBUTTONDOWN :
-                    *isMouseButtonPressed = true;
-                    printf("x : %d, y : %d \n", event.motion.x, event.motion.y);
-                    printf("Case x : %d, Case y : %d \n", GET_CASE_FROM_COORD_X(event.motion.x), GET_CASE_FROM_COORD_Y(event.motion.y));
-
-
-
-
-                    continue;
-
-                case SDL_MOUSEBUTTONUP :
-                    *isMouseButtonPressed = false;
-                    if (!(*MOUSE_MOVING))
-                    {
-                        CASE_CLICK_DISPLAY(var, event.motion.x, event.motion.y);
-                    }
-                    *MOUSE_MOVING = false;
-                    
-                    continue;
-
-
-
-                case SDL_QUIT :
-                    return SDL_FALSE;
-                    break;
-
-
-                default:
-                    break;
+                continue;
             }
+        
+        case SDL_KEYUP : 
+            switch (event.key.keysym.sym)
+            {
+
+            case SDLK_b:
+                var->KEY_DOWN_STATUS[SDLK_b] = 0;
+                continue;
+
+            case SDLK_c:
+                var->KEY_DOWN_STATUS[SDLK_c] = 0;
+                continue;
+
+            
+            case SDLK_q:
+                var->KEY_DOWN_STATUS[SDLK_q] = 0;
+                continue;
+
+            case SDLK_d:
+                var->KEY_DOWN_STATUS[SDLK_d] = 0;
+                continue;
+            
+            case SDLK_z:
+                var->KEY_DOWN_STATUS[SDLK_z] = 0;
+                continue;
+
+            case SDLK_s:
+                var->KEY_DOWN_STATUS[SDLK_s] = 0;
+                continue;
+            
+            case SDLK_LCTRL:
+                var->KEY_DOWN_STATUS[SDLK_LCTRL] = 0;
+                continue;
+            
+            
+            default:
+                continue;
+            }
+
+        case SDL_MOUSEMOTION :
+            //printf("Coordonée : (%d, %d) \n", event.motion.x, event.motion.y);
+            //printf("Vitesse : (%d, %d) \n", event.motion.xrel, event.motion.yrel);
+            
+            // ----- Actualisation du déplacement de la caméra avec la souris ----- //
+            if(*isMouseButtonPressed && event.motion.x <= GRID_DISP_WIDTH && event.motion.y <= GRID_DISP_HEIGHT){
+                MOVE_CAMERA_MOUSE(var->renderer, var->texture, var->camera, event.motion.xrel, event.motion.yrel);
+                *MOUSE_MOVING = true;
+            }
+
+            continue;
+
+        case SDL_MOUSEBUTTONDOWN :
+            *isMouseButtonPressed = true;
+            printf("x : %d, y : %d \n", event.motion.x, event.motion.y);
+            printf("Case x : %d, Case y : %d \n", GET_CASE_FROM_COORD_X(event.motion.x), GET_CASE_FROM_COORD_Y(event.motion.y));
+
+            continue;
+
+        case SDL_MOUSEBUTTONUP :
+            *isMouseButtonPressed = false;
+            if (!(*MOUSE_MOVING))
+            {
+                CASE_CLICK_DISPLAY(var, event.motion.x, event.motion.y);
+            }
+            *MOUSE_MOVING = false;
+            
+            continue;
+
+
+
+        case SDL_QUIT :
+            return SDL_FALSE;
+            break;
+
+
+        default:
+            break;
+    }
     }
     
     // Évolution continue
     if(var->KEY_DOWN_STATUS[SDLK_RETURN] == 1){
         evolution(var);
-        bool ACTION = (event.type == SDL_MOUSEBUTTONUP)
+        bool ACTION;
+        for(int i = 0; i < velocity; i++){
+            SDL_Delay(1);
+            ACTION = (event.type == SDL_MOUSEBUTTONUP)
                     || (event.type == SDL_MOUSEBUTTONDOWN)
                     || (event.type == SDL_MOUSEMOTION)
                     || (event.type == SDL_KEYUP)
                     || (event.type == SDL_KEYDOWN);
-        for(int i = 0; i < velocity; i++){
-            SDL_Delay(1);
-            if(ACTION){
+            if (ACTION){
                 
             }
         }
